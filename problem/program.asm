f9cf559f20f35dacc6731759484551c4492308ec
EmilioJose <emilio.jimenez@upb.edu.co>
2022-02-10 11:34:34 -0500

    Marqué mi trabajo

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+//Emilio José Jiménez Delos Ríos
+// 000149647
+// emilio.jimenez@upb.edu.co
 (START)
     @START
     0;JMP
