aac40dc794b8d23bcb7e1924e4a696371e92e579
EmilioJose <emilio.jimenez@upb.edu.co>
2022-02-13 12:35:23 -0500

    Declaracion de variables

--- problem/program.asm
+++ problem/program.asm
@@ -2,5 +2,24 @@
 // 000149647
 // emilio.jimenez@upb.edu.co
 (START)
-    @START
+    @16384  //Declaro variable inicial R0 = 16384
+    D=A
+    @0
+    M=D
+    @24575  //Declaro variable Final R1 = 24575
+    D=A
+    @1
+    M=D
+    @70  //Declaro variable F = 70 R2 = 70
+    D=A
+    @2
+    M=D
+    @67  //Declaro variable c = 67 R3 = 67
+    D=A
+    @3
+    M=D
+    @24576  //Declaro variable DeDondetomoValoresDeLetras R4 = 24576
+    D=A
+    @4
+    M=D
     0;JMP
